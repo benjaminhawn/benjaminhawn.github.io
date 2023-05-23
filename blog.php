77d423b98605607de3f13c53e55acc6ae40d1f83
Benjamin Hawn <101732315+benjaminhawn@users.noreply.github.com>
2023-05-23 14:18:13 -0400

    Update and rename blog.html to blog.php

---
+++ blog.php
@@ -0,0 +1,29 @@
+<?php
+
+include('header.php');
+
+?>
+
+<!DOCTYPE html>
+
+<p style = "text-align:center;">
+<br>
+* * BLOG * *
+</p>
+
+<p style = "text-align:left;">
+
+2022-11-17: <a href="https://benjaminhawn.com/blog/my-first-blog-post" target="_self" style="color:#ce2029;text-decoration:none">"My First Blogpost"</a><br><br>
+
+</p>
+  
+<p style = "text-align:center;">
+
+CONTACT: <a href=mailto:ben@benjaminhawn.com style="color:#000000;text-decoration:none">ben@benjaminhawn.com</a><br><br>
+
+(c) 2022
+</p>
+
+</body>
+
+</html>
