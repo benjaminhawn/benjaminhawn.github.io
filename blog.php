d3b6ce0a01065df8780434482dfbe2aa99ca1f6c
Benjamin Hawn <101732315+benjaminhawn@users.noreply.github.com>
2023-05-23 14:29:04 -0400

    Update blog.php

--- blog.php
+++ blog.php
@@ -1,8 +1,30 @@
-<?php
+<!DOCTYPE html>
 
-include('header.php');
+<html>
 
-?>
+<head>
+  <title>Benjamin Hawn</title>
+  <meta name='viewport' content='width=device-width, initial-scale=1' />
+  <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+
+<body>
+
+<p style = "text-align:center;">
+---------------------------------------<br>
+
+-------- <a href="https://benjaminhawn.com" target="_self" style="color:#000000;text-decoration:none">"BENJAMIN SCOTT HAWN"</a> --------<br>
+
+---------------------------------------<br>
+
+--- <a href="https://twitter.com/benjaminhawn" target="_blank" style="color:#1DA1F2;text-decoration:none">twitter</a> ---------------------------<br>
+
+--------------- <a href="https://github.com/benjaminhawn" target="_blank" style="color:#6e5494;text-decoration:none">github</a> ----------------<br>
+
+-------------------------- <a href="https://linkedin.com/in/bhawn" target="_blank" style="color:#0072B1;text-decoration:none">linkedin</a> ---<br>
+
+---------------------------------------<br>
+</p>
 
 <!DOCTYPE html>
 
